d2d5beb40b290870cebb6f28256a10eb1be4365d
Nicolas Kirk <nicolask.pires@gmail.com>
2021-07-24 15:10:54 -0300

    Criando operacao de instanciacao

--- src/main/java/com/nksolucoes/ordersystem/OrdersystemApplication.java
+++ src/main/java/com/nksolucoes/ordersystem/OrdersystemApplication.java
@@ -1,13 +1,32 @@
 package com.nksolucoes.ordersystem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nksolucoes.ordersystem.domain.Categoria;
+import com.nksolucoes.ordersystem.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class OrdersystemApplication {
+public class OrdersystemApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersystemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
