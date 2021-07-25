558336bc6a7f2afa741883a14f9ed69df0a29e97
Nicolas Kirk <nicolask.pires@gmail.com>
2021-07-25 10:33:49 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/nksolucoes/ordersystem/domain/Cidade.java
+++ src/main/java/com/nksolucoes/ordersystem/domain/Cidade.java
@@ -9,53 +9,48 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Cidade implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	private String name;
-	
+	private String nome;
+
+	@JsonManagedReference
 	@ManyToOne
-	@JoinColumn(name = "estado_id")
+	@JoinColumn(name="estado_id")
 	private Estado estado;
 	
 	public Cidade() {
 	}
-
-	public Cidade(Integer id, String name, Estado estado) {
+	public Cidade(Integer id, String nome, Estado estado) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.nome = nome;
 		this.estado = estado;
 	}
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-	public String getName() {
-		return name;
+	public String getNome() {
+		return nome;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
-
 	public Estado getEstado() {
 		return estado;
 	}
-
 	public void setEstado(Estado estado) {
 		this.estado = estado;
 	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -63,7 +58,6 @@ public class Cidade implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -79,6 +73,8 @@ public class Cidade implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
-
+	}
+	
+	
+	
 }
