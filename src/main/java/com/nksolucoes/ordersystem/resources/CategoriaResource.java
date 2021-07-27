9dd776263daf60340d1e5eae3c3277dc0e292d5b
Nicolas Pires <nicolask.pires@gmail.com>
2021-07-26 22:43:39 -0300

    Ajustes pontuais e gitignore

--- src/main/java/com/nksolucoes/ordersystem/resources/CategoriaResource.java
+++ src/main/java/com/nksolucoes/ordersystem/resources/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.nksolucoes.ordersystem.resources;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
